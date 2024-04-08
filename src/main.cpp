3e987cf634b007b06e2b8c8bb8cb6027cf8f0ad9
mmiletic5 <mi22366@alas.matf.bg.ac.rs>
2024-04-08 14:05:26 +0200

    Sredjeni modeli i izbrisan pokusaj HDR i Bloom efekta

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,6 @@ unsigned int loadCubemap(vector<std::string> faces);
 
 unsigned int loadTexture(const char *path);
 
-void renderQuad();
 
 // settings
 const unsigned int SCR_WIDTH = 1200;
@@ -98,10 +97,6 @@ struct ProgramState {
     //light settings
     bool blinn = false;
     bool blinnKeyPressed = false;
-
-    bool bloom = false;
-    bool bloomKeyPressed = false;
-    float exposure = 1.0f;
 
     PointLight pointLight;
     ProgramState()
@@ -209,29 +204,26 @@ int main() {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 
-    // build and compile shaders
-
-    Shader ourShader("resources/shaders/2.model_lighting.vs", "resources/shaders/2.model_lighting.fs");
-    Shader skyboxShader("resources/shaders/skybox.vs","resources/shaders/skybox.fs");
-    Shader shaderBlur("resources/shaders/blur.vs", "resources/shaders/blur.fs");
-    Shader shaderBloom("resources/shaders/bloom.vs", "resources/shaders/bloom.fs");
-    Shader blendingShader("resources/shaders/blending.vs", "resources/shaders/blending.fs");
-    // load models
-
-
-    Model ourModel("resources/objects/gary/gary.obj");
-    ourModel.SetShaderTextureNamePrefix("material.");
-
     //facecull
     glEnable(GL_CULL_FACE);
     glCullFace(GL_FRONT);
     glFrontFace(GL_CW);
 
+    // build and compile shaders
+    Shader ourShader("resources/shaders/2.model_lighting.vs", "resources/shaders/2.model_lighting.fs");
+    Shader skyboxShader("resources/shaders/skybox.vs","resources/shaders/skybox.fs");
+    Shader blendingShader("resources/shaders/blending.vs", "resources/shaders/blending.fs");
+
+    // load models
+    Model ourModel("resources/objects/gary/gary.obj");
+    ourModel.SetShaderTextureNamePrefix("material.");
+
+    stbi_set_flip_vertically_on_load(false);
+
     Model house("resources/objects/house/house.obj");
     house.SetShaderTextureNamePrefix("material.");
 
-    glDisable(GL_CULL_FACE);
-
+    stbi_set_flip_vertically_on_load(true);
     Model patrick("resources/objects/patrick/patrick.obj");
     patrick.SetShaderTextureNamePrefix("material.");
 
@@ -241,72 +233,16 @@ int main() {
     Model sponge("resources/objects/sponge/sponge.obj");
     sponge.SetShaderTextureNamePrefix("material.");
 
+    stbi_set_flip_vertically_on_load(false);
     Model krusty("resources/objects/krusty/krusty.obj");
     krusty.SetShaderTextureNamePrefix("material.");
+    stbi_set_flip_vertically_on_load(true);
 
     Model krabs("resources/objects/krabs/krabs.obj");
     krabs.SetShaderTextureNamePrefix("material.");
 
     Model karen("resources/objects/karen/karenbyanto.obj");
     karen.SetShaderTextureNamePrefix("material.");
-
-    //BLOOM
-    // configure framebuffers
-    unsigned int hdrFBO;
-    glGenFramebuffers(1, &hdrFBO);
-    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
-    // create 2 color buffers (1 for normal rendering, other for brightness threshold values)
-    unsigned int colorBuffers[2];
-    glGenTextures(2, colorBuffers);
-    for (unsigned int i = 0; i < 2; i++)
-    {
-        glBindTexture(GL_TEXTURE_2D, colorBuffers[i]);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);  // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        // attach texture to framebuffer
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
-    }
-    // create and attach depth buffer (renderbuffer)
-    unsigned int rboDepth;
-    glGenRenderbuffers(1, &rboDepth);
-    glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_WIDTH, SCR_HEIGHT);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
-    // tell OpenGL which color attachments we'll use (of this framebuffer) for rendering
-    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
-    unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
-    glDrawBuffers(2, attachments);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffers[0], 0); //<=
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);//<=
-    // check if framebuffer is complete
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-        std::cout << "Framebuffer not complete!" << std::endl;
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    // ping-pong-framebuffer for blurring
-    unsigned int pingpongFBO[2];
-    unsigned int pingpongColorbuffers[2];
-    glGenFramebuffers(2, pingpongFBO);
-    glGenTextures(2, pingpongColorbuffers);
-    for (unsigned int i = 0; i < 2; i++)
-    {
-        glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[i]);
-        glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[i]);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, pingpongColorbuffers[i], 0);
-        // also check if framebuffers are complete (no need for depth buffer)
-        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-            std::cout << "Framebuffer not complete!" << std::endl;
-    }
-
-
 
     PointLight& pointLight = programState->pointLight;
     pointLight.position = glm::vec3(4.0f, 4.0, 0.0);
@@ -426,12 +362,6 @@ int main() {
     unsigned int cubemapTexture = loadCubemap(faces);
 
 
-    shaderBlur.use();
-    shaderBlur.setInt("image", 0);
-    shaderBloom.use();
-    shaderBloom.setInt("scene", 0);
-    shaderBloom.setInt("bloomBlur", 1);
-
     // render loop
 
     while (!glfwWindowShouldClose(window)) {
@@ -448,9 +378,6 @@ int main() {
         // render
 
         glClearColor(programState->clearColor.r, programState->clearColor.g, programState->clearColor.b, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // don't forget to enable shader before setting uniforms
@@ -480,6 +407,7 @@ int main() {
 
         ourShader.setBool("blinn",programState->blinn);
 
+        glDisable(GL_CULL_FACE);
         // render the loaded model
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model,
@@ -487,7 +415,6 @@ int main() {
         model = glm::scale(model, glm::vec3(programState->garyScale));    // it's a bit too big for our scene, so scale it down
         ourShader.setMat4("model", model);
         ourModel.Draw(ourShader);
-
 
         // render the house model
         model = glm::mat4(1.0f);
@@ -498,7 +425,6 @@ int main() {
         model = glm::scale(model, glm::vec3(programState->houseScale));    // it's a bit too big for our scene, so scale it down
         ourShader.setMat4("model", model);
         house.Draw(ourShader);
-
 
         // render the patrick model
         model = glm::mat4(1.0f);
@@ -520,13 +446,14 @@ int main() {
         ourShader.setMat4("model", model);
         squid.Draw(ourShader);
 
+        glEnable(GL_CULL_FACE);
         // render the sponge model
         model = glm::mat4(1.0f);
         model = glm::translate(model,programState->spongePosition); // translate it down so it's at the center of the scene
         model = glm::scale(model, glm::vec3(programState->spongeScale));    // it's a bit too big for our scene, so scale it down
         ourShader.setMat4("model", model);
         sponge.Draw(ourShader);
-
+        glDisable(GL_CULL_FACE);
 
         // render the krusty model
         model = glm::mat4(1.0f);
@@ -562,6 +489,7 @@ int main() {
             glDrawArrays(GL_TRIANGLES, 0, 6);
         }
 
+
         // draw skybox
         glDepthMask(GL_FALSE);
         glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
@@ -583,35 +511,6 @@ int main() {
         if (programState->ImGuiEnabled)
             DrawImGui(programState);
 
-        // blur bright fragments with two-pass Gaussian Blur
-
-        bool horizontal = true, first_iteration = true;
-        unsigned int amount = 5;
-        shaderBlur.use();
-        for (unsigned int i = 0; i < amount; i++)
-        {
-            glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[horizontal]);
-            shaderBlur.setInt("horizontal", horizontal);
-            glBindTexture(GL_TEXTURE_2D, first_iteration ? colorBuffers[1] : pingpongColorbuffers[!horizontal]);  // bind texture of other framebuffer (or scene if first iteration)
-            renderQuad();
-            horizontal = !horizontal;
-            if (first_iteration)
-                first_iteration = false;
-        }
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-        // now render floating point color buffer to 2D quad and tonemap HDR colors to default framebuffer's (clamped) color range
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        shaderBloom.use();
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, colorBuffers[0]);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[!horizontal]);
-        shaderBloom.setInt("bloom", programState->bloom);
-        shaderBloom.setFloat("exposure", programState->exposure);
-        renderQuad();
-
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 
@@ -623,6 +522,7 @@ int main() {
     glDeleteVertexArrays(1,&skyboxVBO);
     glDeleteVertexArrays(1,&transparentVAO);
     glDeleteVertexArrays(1,&transparentVBO);
+
 
     programState->SaveToFile("resources/program_state.txt");
     delete programState;
@@ -649,6 +549,7 @@ void processInput(GLFWwindow *window) {
         programState->camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         programState->camera.ProcessKeyboard(RIGHT, deltaTime);
+
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -761,15 +662,6 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
     {
         programState->blinnKeyPressed = false;
     }
-    if(glfwGetKey(window,GLFW_KEY_H) == GLFW_PRESS and !programState->bloomKeyPressed)
-    {
-        programState->bloom = !programState->bloom;
-        programState->bloomKeyPressed = true;
-    }
-    if(glfwGetKey(window,GLFW_KEY_H) == GLFW_RELEASE)
-    {
-        programState->bloomKeyPressed = false;
-    }
 
 }
 
@@ -801,37 +693,6 @@ unsigned int loadCubemap(vector<std::string> faces)
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
     return textureID;
-}
-
-// renderQuad() renders a 1x1 XY quad in NDC
-
-unsigned int quadVAO = 0;
-unsigned int quadVBO;
-void renderQuad()
-{
-    if (quadVAO == 0)
-    {
-        float quadVertices[] = {
-                // positions        // texture Coords
-                -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
-                -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-                1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
-                1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-        };
-        // setup plane VAO
-        glGenVertexArrays(1, &quadVAO);
-        glGenBuffers(1, &quadVBO);
-        glBindVertexArray(quadVAO);
-        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    }
-    glBindVertexArray(quadVAO);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    glBindVertexArray(0);
 }
 
 unsigned int loadTexture(char const * path)
